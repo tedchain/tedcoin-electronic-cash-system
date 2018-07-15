b2ed5d32c0f2fd4ec779019fb3081324f2c5d083
HOWARD LARRISON <development@daybreak.coffee>
2018-07-15 20:37:05 +0700

    Update pyminer.py

--- contrib/pyminer/pyminer.py
+++ contrib/pyminer/pyminer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright (c) 2011 The Bitcoin developers
+# Copyright (c) 2011 The Bitcoin developers.
 # Distributed under the MIT/X11 software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #
